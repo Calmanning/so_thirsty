0683b93a4cfeff04791da44e083108c89a2dae2b
That Guy Cal <calmanning@gmail.com>
2021-01-28 15:53:39 -0800

    adjusted route url '/api/trefGet' and added note.

--- controllers/thirsty_controller.js
+++ controllers/thirsty_controller.js
@@ -16,20 +16,7 @@ const axios = require('axios')
 
 
 //test axios api
-router.get("/test", function(req, res) {
-    const trefKEY = "RFxyA90U90mDUshDMP8y-PiyRafTF254xr72BbWqlPQ"
-    const plantId = "139820"
 
-    axios.get(`https://trefle.io/api/v1/plants/${plantId}?token=${trefKEY}`)
-    .then((response) => {
-        console.log(response.data);
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response.headers);
-        console.log(response.config);
-        res.status(200).send(response.data);
-  });
-})
 
 router.get("/addplant", function (req, res) {
     res.render("new-plant");
@@ -72,6 +59,22 @@ router.get("/api/search/:plantName", function (req, res) {
 // route for add plant page
 
 
+//axios get request to trefle based on plant id
+router.get("/api/trefGet", function(req, res) {
+    const trefKEY = "RFxyA90U90mDUshDMP8y-PiyRafTF254xr72BbWqlPQ"
+    const plantId = "139820"
+
+    axios.get(`https://trefle.io/api/v1/plants/${plantId}?token=${trefKEY}`)
+    .then((response) => {
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
+        res.status(200).send(response.data);
+  });
+})
+
 
 //CREATE a new plant for the user
 router.post("/api/plant", function (req, res) {
@@ -95,5 +98,6 @@ router.delete("/api/:user/plant/:plant", function (req, res) {
     })
     res.json(tempData.userPlantPhotos.plants)
 })
+
 
 module.exports = router;
