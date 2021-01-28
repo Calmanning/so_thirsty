5d0a50aa6537f4c0306fc7c9b256bae93559297e
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-01-27 16:32:13 -0800

    added user registration ajax call

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,31 @@
 // Client js api calls
+$(document).ready(function () {
+    var userName = $("#user-name");
+    var password = $("#password");
+    var confirmPass = $("#confirm-password")
+    // TODO: Register user
+    $("#user-input").on("Submit", function () {
+        if (userName.val() && password.val() && confirmPass.val()) {
+            $.ajax({
+                method: "POST",
+                url: "/api/user",
+                data: {
+                    userName: userName.val(),
+                    password: password.val()
+                }
+            }).then(function () {
+                window.location.href = "/" + userName.val()
+            })
+        };
+    });
+    // TODO: Sign in user
+    // TODO: Get user info
+    // TODO: Treffle API plant search
+    // TODO: Create plant
+    // TODO: Upload photo
+    // TODO: Fill create form
 
-$(document).ready(function() {
-  
-  
+
+
+
 });
