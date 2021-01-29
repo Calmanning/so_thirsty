b1e1a2ee8fb8e9cac5400768f9bbf8db4e6ad764
Drantho <drantho@gmail.com>
2021-01-29 12:50:04 -0800

    user authentication added

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,6 +2,7 @@
 
 // Client js api calls
 $(document).ready(function () {
+    var name = $("#name")
     var userName = $("#userName");
     var password = $("#password");
     var confirmPass = $("#confirmPassword");
@@ -17,14 +18,16 @@ $(document).ready(function () {
     var resultsBox = $("#resultsBox")
 
     // Register user
-    $("#new-user").on("Submit", function (event) {
+    $("#newUser").on("submit", function (event) {
         event.preventDefault();
+        alert("HELLOOOO?")
         if (userName.val() && password.val() && confirmPass.val()) {
             console.log("testing on script.js. the user name: " + userName)
             $.ajax({
                 method: "POST",
                 url: "/api/user",
                 data: {
+                    name: name.val(),
                     userName: userName.val(),
                     password: password.val()
                 }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const session = require("express-session");
 
 var db = require("./models");
 
@@ -17,13 +18,25 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
+app.use(session({
+  secret: "this is a secret you know what i mean",
+  // TODO replace with environment variable for production
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000*60*60*2
+    //TODO add secure cookie for production
+  }
+}))
+
 var routes = require("./controllers/thirsty_controller.js");
+
 
 app.use(routes);
 
 
 var PORT = process.env.PORT || 3000;
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: false}).then(function() {
   app.listen(PORT, function() {
     console.log(`App now listening on port: ${PORT} view at: http://localhost:${PORT}`);
   });
